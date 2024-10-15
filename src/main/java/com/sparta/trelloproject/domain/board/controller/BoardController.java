7fef6d947fce31aee305acccbf05f5ca45c582cf
Robbie <iseul1121@gmail.com>
2024-10-15 13:44:03 +0900

    [FEAT] 코드 초반 세팅

---
+++ src/main/java/com/sparta/trelloproject/domain/board/controller/BoardController.java
@@ -0,0 +1,9 @@
+package com.sparta.trelloproject.domain.board.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class BoardController {
+}
---
+++ src/main/java/com/sparta/trelloproject/domain/board/service/BoardService.java
@@ -0,0 +1,19 @@
+package com.sparta.trelloproject.domain.board.service;
+
+import com.sparta.trelloproject.domain.board.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+public class BoardService {
+//
+//    private final BoardRepository boardRepository;
+//    private final UserService userService;
+//
+//    public BoardService(BoardRepository boardRepository, UserService userService) {
+//        this.boardRepository = boardRepository;
+//        this.userService = userService;
+//    }
+
+}
