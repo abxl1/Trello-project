3ebfafe231284485e71be2e552efe497f6c54d8f
KangDongJoon <ehdwns1119@naver.com>
2024-10-15 13:37:41 +0900

    [FEAT] CustomException 추가

---
+++ src/main/java/com/sparta/trelloproject/common/exception/ErrorCode.java
@@ -0,0 +1,85 @@
+package com.sparta.trelloproject.common.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+    /*
+    ex)
+    예외명(대문자+'_') + (HttpStatus.오류코드, "메세지")
+    STORE_FORBIDDEN(HttpStatus.FORBIDDEN,"사장님 권한을 가진 사용자만 가게를 생성할 수 있습니다."),
+    STORE_BAD_REQUEST(HttpStatus.BAD_REQUEST,"사장님은 최대 3개의 가게까지만 운영할 수 있습니다."),
+    STORE_NOT_FOUND(HttpStatus.NOT_FOUND,"해당 가게를 찾을 수 없습니다."),
+
+
+    throw new CustomException(ErrorCode.STORE_FORBIDDEN);
+     */
+
+
+    // Token ErrorCode
+
+
+
+
+    // User ErrorCode
+
+
+
+    // Member ErrorCode
+
+
+
+
+    // Worksapce ErrorCode
+
+
+
+
+    // Board Errorcode
+
+
+
+
+    // List Errorcode
+
+
+
+
+    // Card Errorcode
+
+
+
+
+    // Comment Errorcode
+
+
+
+
+    // File Errorcode
+
+
+
+
+    // Alarm Errorcode
+
+
+
+
+    // Search Errorcode
+
+
+
+
+    // 아래 코드 위에 ErrorCode 작성
+    NOT_FOUND(HttpStatus.NOT_FOUND, "찾지못했습니다.");
+
+
+    private final HttpStatus status;
+    private final String message;
+
+    ErrorCode(HttpStatus httpStatus, String message){
+        this.status = httpStatus;
+        this.message = message;
+    }
+}
