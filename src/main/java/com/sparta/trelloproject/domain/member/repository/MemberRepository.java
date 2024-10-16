28945268efcc62f0876ad03f4c92e318fcef2697
leejaehee0807 <leejaehee0807@naver.com>
2024-10-17 00:22:46 +0900

    [FEAT] Comment 기능 및 Comment테스트코드 구현

--- src/main/java/com/sparta/trelloproject/domain/member/repository/MemberRepository.java
+++ src/main/java/com/sparta/trelloproject/domain/member/repository/MemberRepository.java
@@ -4,8 +4,12 @@ import com.sparta.trelloproject.domain.member.entity.Member;
 import com.sparta.trelloproject.domain.member.enums.Assign;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByUserId(Long userId);
 
     boolean existsByAssign(Assign assign);
+
+    Optional<Member> findByUserId(Long userId);
 }
