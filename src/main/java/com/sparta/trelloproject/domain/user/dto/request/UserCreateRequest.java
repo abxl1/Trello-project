4900cd2774bf125624b6bdd270ca75d13807afd1
Park_Yong_Jun <dydwnsl02@naver.com>
2024-10-17 19:18:21 +0900

    [ADD] 워크스페이스 테스트 코드 작성

--- src/main/java/com/sparta/trelloproject/domain/user/dto/request/UserCreateRequest.java
+++ src/main/java/com/sparta/trelloproject/domain/user/dto/request/UserCreateRequest.java
@@ -1,10 +1,12 @@
 package com.sparta.trelloproject.domain.user.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserCreateRequest {
   private String title;
   private String explaination;
