183d06a58edc5b55b46a654f5fcf015515d886ce
Park_Yong_Jun <dydwnsl02@naver.com>
2024-10-18 13:21:08 +0900

    [ADD] 첨부파일 추가 및 삭제 기능 구현, 환경변수처리

--- src/main/java/com/sparta/trelloproject/config/S3Config.java
+++ src/main/java/com/sparta/trelloproject/config/S3Config.java
@@ -4,15 +4,22 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class S3Config {
   // AWS
-  private final String accessKey = "AKIATQZCSKGDHVOKXJ4O";
-  private final String secretKey = "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";
-  private final String region = "ap-northeast-2"; // 예: "us-west-2"
+  @Value("${cloud.aws.credentials.accessKey}")
+  private String accessKey;
+
+  @Value("${cloud.aws.credentials.secretKey}")
+  private String secretKey;
+
+  @Value("${cloud.aws.region.static}")
+  private String region;
+
 
   @Bean
   public AmazonS3 amazonS3() {
